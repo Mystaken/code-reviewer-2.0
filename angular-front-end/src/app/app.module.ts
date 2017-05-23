c3461ae4cb2f48e7c72bcdd695014b52ba4ca952
BoZhaoUTSC <bodotzhao@gmail.com>
2017-05-22 22:15:14 -0400

    material design/animation checkbox

--- angular-front-end/src/app/app.module.ts
+++ angular-front-end/src/app/app.module.ts
@@ -2,8 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 import { AppComponent } from './app.component';
+import { MdCheckboxModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -13,7 +18,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    JsonpModule
+    JsonpModule,
+    BrowserAnimationsModule,
+    MdCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
