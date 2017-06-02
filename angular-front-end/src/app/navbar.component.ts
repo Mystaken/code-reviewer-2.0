c8f92b2378ec4b336f2596bae2f6e8442a4ec680
Andrew Wang <me@andrewwang.ca>
2017-06-02 17:07:00 -0400

    Navigation bar redesign

--- angular-front-end/src/app/navbar.component.ts
+++ angular-front-end/src/app/navbar.component.ts
@@ -6,32 +6,25 @@ import { Http } from '@angular/http';
   <div class="navbar">
 
     <div class="stage-buttons">
-      <div *ngFor="let tab of tabs">
-        <button md-raised-button [mdMenuTriggerFor]="menu" class="ui blue button">
-            {{ tab.tab_name }}
-            <a class="ui orange circular label">2</a>
-        </button>
+      <ul>
+        <li *ngFor="let tab of tabs" md-raised-button [mdMenuTriggerFor]="menu">
+          <a href="#">{{ tab.tab_name }}</a>
 
-        <!-- dropdown -->
-        <md-menu #menu="mdMenu">
-          <button
-            md-menu-item
-            *ngFor="let sub_tab_name of tab.sub_tab_names"
-            (click)="on_click(tab.tab_name ,sub_tab_name.id);"
-            >
-            <md-icon>dialpad</md-icon>
-            <span>{{ sub_tab_name.name }}</span>
-          </button>
-        </md-menu>
-      </div>
+          <!-- dropdown -->
+          <md-menu #menu="mdMenu">
+            <button
+              md-menu-item
+              *ngFor="let sub_tab_name of tab.sub_tab_names"
+              (click)="on_click(tab.tab_name ,sub_tab_name.id);"
+              >
+              <md-icon>dialpad</md-icon>
+              <span>{{ sub_tab_name.name }}</span>
+            </button>
+          </md-menu>
+        </ul>
     </div>
-    <div class="utility-buttons">
-      <button md-raised-button class="ui blue button">
-        {{ username }}
-      </button>
-      <button md-raised-button class="ui blue button">
-        Logout
-      </button>
+    <div class="user-info">
+        {{ username }}&nbsp;(<a href="#">Logout</a>)
     </div>
   </div>
   `,
@@ -44,8 +37,74 @@ import { Http } from '@angular/http';
       text-align: left;
       display: flex;
     }
-    .utility-buttons {
-      text-align: right;
+    .user-info {
+      margin: 18px 20px 0 0;
+    }
+
+    ul {
+      margin: 0px;
+      padding: 0px;
+    }
+
+    ul li {
+      display: inline-block;
+      height: 50px;
+      line-height: 50px;
+      width: 100px;
+      margin: 0 10px 0 0;
+      text-indent: 35px;
+      position: relative;
+    }
+
+    ul li:before {
+      content: " ";
+      height: 0;
+      width: 0;
+      position: absolute;
+      left: -2px;
+      border-style: solid;
+      border-width: 25px 0 25px 25px;
+      border-color: transparent transparent transparent #fff;
+      z-index: 0;
+    }
+
+    ul li:first-child:before {
+      border-color: transparent;
+    }
+
+    ul li a:after {
+      content: " ";
+      height: 0;
+      width: 0;
+      position: absolute;
+      right: -25px;
+      border-style: solid;
+      border-width: 25px 0 25px 25px;
+      border-color: transparent transparent transparent #ccc;
+      z-index: 10;
+    }
+
+    ul li.active a {
+      background: #4fc3f7;
+      z-index: 100;
+    }
+
+    ul li.active a:after {
+      border-left-color: #4fc3f7;
+    }
+
+    ul li a {
+      display: block;
+      background: #ccc;
+      padding-left: 5px;
+    }
+
+    ul li a:hover {
+      background: #4fc3f7;
+    }
+
+    ul li a:hover:after {
+      border-color: transparent transparent transparent #4fc3f7;
     }
 
   `]
@@ -59,7 +118,7 @@ export class NavbarComponent {
   color = ["Red", "Orange", "Yellow", "Olive", "Green"];
   username = "The Student's Name"
   tabs = [{
-    tab_name: "My Work",
+    tab_name: "Submit",
     sub_tab_names: [
       {
         id: "workId:kkasdlkfjasldkfj",
@@ -73,7 +132,7 @@ export class NavbarComponent {
       }
     ]
   },{
-    tab_name: "Review My Own Work",
+    tab_name: "Review",
     sub_tab_names: [
       {
         id: "workId:kkasdlkfjasldkfj",
@@ -84,18 +143,7 @@ export class NavbarComponent {
       }
     ]
   }, {
-    tab_name: "Review My Peers",
-    sub_tab_names: [
-      {
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 0"
-      },{
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 1"
-      }
-    ]
-  }, {
-    tab_name: "How My Code Is Reviewed",
+    tab_name: "Reflect",
         sub_tab_names: [
       {
         id: "workId:kkasdlkfjasldkfj",
@@ -106,7 +154,7 @@ export class NavbarComponent {
       }
     ]
   }, {
-    tab_name: "Quality of My Reviews",
+    tab_name: "Marks",
         sub_tab_names: [
       {
         id: "workId:kkasdlkfjasldkfj",
