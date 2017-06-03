256ce1bea8d3399832699125b52f0d83eff7203b
BoZhaoUTSC <bodotzhao@gmail.com>
2017-06-03 03:28:48 -0400

    add works table

---
+++ angular-front-end/src/app/work-table.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core'
+
+@Component({
+	selector: 'work-table',
+	template: `
+		<table class="ui selectable inverted table">
+			<thead>
+				<tr>
+					<th>Work Name</th>
+					<th>Status</th>
+					<th>Number of Peers</th>
+					<th>Next Deadline</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr *ngFor="let row of table_content">
+					<td *ngFor="let data of row"> 
+						{{ data }}
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	`,
+	styles: [`
+	`]
+})
+
+export class WorkTableComponent {
+	table_content = [
+		["A0", "Marks Released (950/950)", 3, "May 31, 2017"],
+		["A1", "Open Submission (678/899)", 3, "June 22, 2017"],
+		["A2", "Created", "-", null]
+	]
+}
