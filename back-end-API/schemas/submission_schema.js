0cb4bbdc8691878480f8fc19de4ad2f0490e954e
Kevin Gao <tianxiang.gao.1996@gmail.com>
2017-06-04 02:01:00 -0500

    removed name

--- back-end-API/schemas/submission_schema.js
+++ back-end-API/schemas/submission_schema.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 
 module.exports = {
     submission_id: mongoose.Schema.Types.ObjectId,
-    name: String,                       // name of this file, e.g. assignment 1, assignment 2
     author_id: mongoose.Schema.Types.ObjectId,        // utorid of the student
     ta_id: mongoose.Schema.Types.ObjectId,            // the TA who is going to mark this assignment
     code: String,                       // file content
