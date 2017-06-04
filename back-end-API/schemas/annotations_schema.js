df6cf2670ab228b8514443e1bb705a0159a12ef2
Kevin Gao <tianxiang.gao.1996@gmail.com>
2017-06-04 00:47:34 -0500

    Changed feedbacks->annotations

--- back-end-API/schemas/annotations_schema.js
+++ back-end-API/schemas/annotations_schema.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports = {
+    annotation_id: mongoose.Schema.Types.ObjectId,
     work_id: mongoose.Schema.Types.ObjectId,
     review_by: mongoose.Schema.Types.ObjectId,          // the student id who writes this review
     annotation: String,
