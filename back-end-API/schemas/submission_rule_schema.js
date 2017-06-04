34808f2c4ae0cfd02943b26e4c97a8b21c2280d3
Kevin Gao <tianxiang.gao.1996@gmail.com>
2017-06-03 22:33:11 -0400

    removed instructors

--- back-end-API/schemas/submission_rule_schema.js
+++ back-end-API/schemas/submission_rule_schema.js
@@ -11,8 +11,6 @@ module.exports = {
     },
     required_files : [String],      // e.g. ["a1_design.py", "a1_testing.py"]
     repo_path: String,              // repo from MarkUs
-    instruction: String,            // some submission instruction about this wrok,
-                                    // e.g. your code must not contain any import statements
     feedback_questions: [String],   // e.g. How descriptive the variable names are
                                     //      Is this function efficient
     student_submission_deadline : Date,
