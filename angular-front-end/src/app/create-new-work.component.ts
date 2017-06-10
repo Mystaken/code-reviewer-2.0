1f13b131b92fc5abf008ee01df96afcfa0388939
Kenny <zb.ca@foxmail.com>
2017-06-10 14:00:40 -0400

    student/work table selectable

--- angular-front-end/src/app/create-new-work.component.ts
+++ angular-front-end/src/app/create-new-work.component.ts
@@ -4,52 +4,54 @@ import { Component } from '@angular/core'
 	selector: 'create-new-work',
 	template: `
 		<div>
-			<md-input-container>
-				<input mdInput placeholder="Work name" value="A0">
-			</md-input-container>
-			<md-select placeholder="Number of peers">
- 				<md-option *ngFor="let num of num_peers_options" [value]="num">
-    				{{ num }}
-  				</md-option>
-			</md-select>
+			<form>
+				<md-input-container>
+					<input mdInput placeholder="Work name" value="A0">
+				</md-input-container>
+				<md-select placeholder="Number of peers">
+	 				<md-option *ngFor="let num of num_peers_options" [value]="num">
+	    				{{ num }}
+	  				</md-option>
+				</md-select>
 
-			<!-- deadlines -->
-			<br>
-			<md-checkbox>
-				Enable student submission:
-			</md-checkbox>
-			<md-input-container>
-				<input mdInput [mdDatepicker]="student_submission" placeholder="Submission deadline">
-				<button mdSuffix [mdDatepickerToggle]="student_submission"></button>
-			</md-input-container>
-			<md-datepicker #student_submission></md-datepicker>
+				<!-- deadlines -->
+				<br>
+				<md-checkbox>
+					Enable student submission:
+				</md-checkbox>
+				<md-input-container>
+					<input mdInput [mdDatepicker]="student_submission" placeholder="Submission deadline">
+					<button mdSuffix [mdDatepickerToggle]="student_submission"></button>
+				</md-input-container>
+				<md-datepicker #student_submission></md-datepicker>
 
-			<br>
-			<md-checkbox>
-				Enable self review:
-			</md-checkbox>
-			<md-input-container>
-				<input mdInput [mdDatepicker]="self_review" placeholder="Self review deadline">
-				<button mdSuffix [mdDatepickerToggle]="self_review"></button>
-			</md-input-container>
-			<md-datepicker #self_review></md-datepicker>
+				<br>
+				<md-checkbox>
+					Enable self review:
+				</md-checkbox>
+				<md-input-container>
+					<input mdInput [mdDatepicker]="self_review" placeholder="Self review deadline">
+					<button mdSuffix [mdDatepickerToggle]="self_review"></button>
+				</md-input-container>
+				<md-datepicker #self_review></md-datepicker>
 
-			<br>
-			<md-checkbox>
-				Enable peer review:
-			</md-checkbox>
-			<md-input-container>
-				<input mdInput [mdDatepicker]="peer_review" placeholder="Peer review deadline">
-				<button mdSuffix [mdDatepickerToggle]="peer_review"></button>
-			</md-input-container>
-			<md-datepicker #peer_review></md-datepicker>
+				<br>
+				<md-checkbox>
+					Enable peer review:
+				</md-checkbox>
+				<md-input-container>
+					<input mdInput [mdDatepicker]="peer_review" placeholder="Peer review deadline">
+					<button mdSuffix [mdDatepickerToggle]="peer_review"></button>
+				</md-input-container>
+				<md-datepicker #peer_review></md-datepicker>
 
-			<div>
-				<button type="submit" class="btn btn-success">Submit</button>
+				<div>
+					<button type="submit" class="btn btn-success">Submit</button>
 
 
-				<button type="reset" class="btn btn-success">Reset</button>
-			</div>
+					<button type="reset" class="btn btn-success">Reset</button>
+				</div>
+			</form>
 
 		</div>
 	`,
