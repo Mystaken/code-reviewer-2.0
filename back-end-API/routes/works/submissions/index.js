36836d5fee8271a5bf2f7e106c3ddc7c63ba0f95
Vincent Tse <from_vincent@hotmail.com>
2017-06-12 12:28:34 -0400

    added api for submissions

--- back-end-API/routes/works/submissions/index.js
+++ back-end-API/routes/works/submissions/index.js
@@ -7,14 +7,16 @@ var validator   = require('../../../lib/validator'),
 
     submissions_model  = require('../../../models/submissions'),
 
-    subs_get_schema    = require('../../../schemas/works/submissions/submissions_get');
-    //subs_put_schema    = require('../../schemas/works/works_put'),
-    //works_post_schema   = require('../../schemas/works/works_post');
+    subs_get_schema    = require('../../../schemas/works/submissions/submissions_get'),
+    //subs_upload_schema = require('../../../schemas/works/submissions/submissions_upload'),
+    subs_post_schema   = require('../../../schemas/works/submissions/submissions_post');
+
 
 
 module.exports = function (router) {
 
     router.route('/').get(function(req, res, next) {
+        //if (!req.session.user) return res.status(403).end("Forbidden");
         validator.validate(req.query, subs_get_schema);
         var error = validator.getLastErrors();
         if (error) return res.requestError({ code: "VALIDATION", message: error });
@@ -32,7 +34,7 @@ module.exports = function (router) {
         }
 
         // if user is a student, it must be his own work, otherwise 404
-        if (req.session_user_type !== 'admin' ) {
+        if (req.session_user_type === 'student' ) {
             match_query.author_id = mongoose.Types.ObjectId(req.session_user_id);
         }
 
@@ -62,10 +64,75 @@ module.exports = function (router) {
         }).catch(function(err) {
             return res.requestError(err);
         });
-    }).put(function(req, res, next) {
-        
     }).post(function(req, res, next) {
-        
+        //if (!req.session.user) return res.status(403).end("Forbidden");
+        var error,   find_query,   update_query;
+
+        validator.validate(req.body, subs_post_schema);
+        error = validator.getLastErrors();
+        if (error) return res.requestError({ code: "VALIDATION", message: error });
+
+        find_query = {
+            _id: mongoose.Types.ObjectId(req.body.submission_id),
+            status: 'active'
+        };
+
+        if (req.session_user_type === 'student' ) {
+            match_query.author_id = mongoose.Types.ObjectId(req.session_user_id);
+        }
+
+        update_query = {
+            code: req.body.code,
+            file_name: req.body.file_name,
+            mark: req.body.mark,
+            status: 'active'
+        }
+
+        //utils.clean(update_query);
+        return submissions_model.find(find_query).exec().then(function(ret) {
+            if (!ret.length) {
+                return Promise.reject({
+                        code: "NOT_FOUND",
+                        params: [ 'submission_id' ]
+                    });
+            }
+            return submissions_model.findOneAndUpdate(find_query, update_query).exec();
+        }).then(function(ret) {
+            res.sendResponse(ret._id);
+        }).catch(function (err) {
+            res.requestError(err);
+        });
+    }).delete(function (req, res, next) {
+        //if (!req.session.user) return res.status(403).end("Forbidden");
+        if (req.session_user_type !== 'admin') res.forbidden();
+        validator.validate(req.body, subs_delete_schema);
+        var error = validator.getLastErrors();
+        if (error) return res.requestError({ code: "VALIDATION", message: error });
+
+        var query = {
+            _id: mongoose.Types.ObjectId(req.body.submission_id),
+        }
+        return submissions_model.find(query).exec().then(function(ret) {
+            if (!ret.length) {
+                return Promise.reject({
+                        code: "NOT_FOUND",
+                        params: [ 'submission_id' ]
+                    });
+            }
+            return submissions_model.findOneAndUpdate(query, {status: 'inactive'}).exec();
+        }).then(function(ret) {
+            res.sendResponse(ret._id);
+        }).catch(function (err) {
+            res.requestError(err);
+        });
+
+    }).all(function (req, res, next) {
+        return res.invalidVerb();
+    });
+
+
+    router.route('/upload/').get(function(req, res, next) {
+        //if (!req.session.user) return res.status(403).end("Forbidden");
     }).all(function (req, res, next) {
         return res.invalidVerb();
     });
