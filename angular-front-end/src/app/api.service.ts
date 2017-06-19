1c89e0efc9bd910b918127cad3c941fc0f486107
Kenny <zb.ca@foxmail.com>
2017-06-18 21:00:03 -0400

    Add create new work service

--- angular-front-end/src/app/api.service.ts
+++ angular-front-end/src/app/api.service.ts
@@ -1,29 +1,40 @@
-import { Http } from '@angular/http';
+import { Headers, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class ApiService {
 
-    constructor(private _http: Http) { }
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(private http: Http) { }
 
 
     getPost(): Observable<any> {
-        return this._http.get("https://jsonplaceholder.typicode.com/posts")
+        return this.http.get("https://jsonplaceholder.typicode.com/posts")
             .map(res => res.json());
     }
 
     // get all the students
     getStudents() {
-        return this._http.get("http://localhost:3000/api/users/students/all")
+        return this.http.get("http://localhost:3000/api/users/students/all")
             .map(res => res.json().data);
     }
 
     // get all the works
     getWorks() {
-        return this._http.get("http://localhost:3000/api/works/all")
+        return this.http.get("http://localhost:3000/api/works/all")
             .map(res => res.json().data);
     }
+
+    // create a new work
+    createNewWork(name: string, num_peers: number) {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        return this.http
+            .post("http://localhost:3000/api/works", JSON.stringify({name: name, num_peers: num_peers}), {headers: this.headers});
+    }
+
+
 
 
 }
