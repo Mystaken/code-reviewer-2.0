c5c4d5f854a53a22172fcd85c68068ebc4550324
Kenny <zb.ca@foxmail.com>
2017-06-27 20:41:52 -0400

    Create data folder

---
+++ back-end-API/routes/users/tas/index.js
@@ -0,0 +1,304 @@
+'use strict';
+
+var validator   = require('../../../lib/validator'),
+    sanitizer   = require('../../../lib/sanitizer'),
+    utils       = require('../../../lib/utils'),
+    mongoose    = require('mongoose'),
+    Promise     = require('bluebird'),
+
+    user_model      = require('../../../models/users'),
+    feedback_model  = require('../../../models/feedbacks'),
+
+    student_get_schema      = require('../../../schemas/users/tas/tas_get'),
+    student_put_schema      = require('../../../schemas/users/tas/tas_put'),
+    student_delete_schema   = require('../../../schemas/users/tas/tas_delete'),
+    student_post_schema     = require('../../../schemas/users/tas/tas_post'),
+    student_all_get_schema  = require('../../../schemas/users/tas/tas_all_get');
+
+module.exports = function (router) {
+    router.route('/').get(function(req, res, next) {
+        var error;
+
+        // validate given query
+        validator.validate(req.query, tas_get_schema);
+        error = validator.getLastErrors();
+        if (error) {
+            return res.requestError({ code: "VALIDATION", message: error });
+        }
+
+        // check if the user's ID is present in the database
+        if (!mongoose.validID(req.query.user_id)) {
+            return res.requestError({
+                code: "NOT_FOUND",
+                params: [ 'user_id' ]
+            });
+        }
+
+
+        return Promise.try(function() {
+            // if this user is an admin, then no rejection
+            if (req.session_user_type === 'admin') {
+                return;
+            }
+
+            // if this user is a TA and he's trying to get other TA's information
+            if (req.session_user_type === 'ta' &&
+                req.session_user_id !== req.query.user_id) {
+                // reject
+                return Promise.reject({
+                    code: "NOT_FOUND",
+                    params: [ 'user_id' ]
+                });
+            }
+
+
+            return user_model.aggregate([
+                    {   // check if query matches below pattern
+                        $match: {
+                            _id: mongoose.Types.ObjectId(req.query.user_id),
+                            user_type: 'ta',
+                            status: 'active'
+                        }
+                    }
+                ]).then(function(result) {
+
+                    if (!result && !result.length) {
+                        return Promise.reject({
+                            code: "NOT_FOUND",
+                            params: [ 'user_id' ]
+                        });
+                    }
+                });
+            }).then(function(ret) {
+                return user_model.aggregate([
+                    {
+                        $match: {
+                            _id: mongoose.Types.ObjectId(req.query.user_id),
+                            status: 'active'
+                        }
+                    },{
+                        $project: {
+                            user_id: "$_id",
+                            _id: 0,
+                            student_number: 1,
+                            first_name: 1,
+                            last_name: 1,
+                            utorid: 1,
+                            email: 1,
+                            last_login: { 
+                                $dateToString: { 
+                                    format: "%Y-%m-%d %H:%M:%S", 
+                                    date: "$last_login" 
+                                }
+                            }
+                        }
+                    }
+                ]).exec();
+            }).then(function(ret) {
+                if (!ret || !ret.length) {
+                    return Promise.reject({
+                        code: "NOT_FOUND",
+                        params: [ 'user_id' ]
+                    });
+                }
+                return res.sendResponse(ret);
+            }).catch(function(err) {
+                return res.requestError(err);
+            });
+
+    }).put(function (req, res, next) {
+        var error;
+        if (req.session_user_type !== 'admin') {
+            return res.forbidden();
+        }
+        validator.validate(req.body, student_put_schema);
+        error = validator.getLastErrors();
+        if (error) {
+            return res.requestError({ code: "VALIDATION", message: error });
+        }
+        // checking if user exists
+        return user_model.aggregate([
+                {
+                    $match: {
+                        $or: [
+                            { utorid: req.body.utorid },
+                            { student_number: req.body.student_number },
+                            { email: req.body.email }
+                        ]
+                    }   
+                }
+            ]).exec().then(function(ret) {
+                // if user exists, return error message.
+                if (ret.length) {
+                    return Promise.reject({
+                        code: "EXISTS",
+                        params: [ 'email', "student_number", "utorid" ]
+                    });
+                }
+                // create new user
+                return new user_model({
+                    first_name:     req.body.first_name,
+                    last_name:      req.body.last_name,
+                    email:          req.body.email,
+                    utorid:         req.body.utorid,
+                    student_number: req.body.student_number,
+                    last_login:     new Date(),
+                    user_type:      'student',
+                    status:         'active'
+                }).save();
+            }).then(function(ret) {
+                res.sendResponse(ret._id);
+            }).catch(function(err) {
+                return res.requestError(err);
+            });
+
+    }).post(function (req, res, next) {
+        var error,
+            query,
+            update_query;
+        if (req.session_user_type !== 'admin' ||
+            req.session_user_type !== 'student') {
+          return res.forbidden();
+        }
+        if (req.session_user_type === 'student' &&
+            req.session_user_id !== req.query.user_id) {
+          return res.requestError({
+              code: 'NOT_FOUND',
+              params: [ 'user_id' ]
+          });
+        }
+
+        validator.validate(req.body, student_post_schema);
+        error = validator.getLastErrors();
+        if (error) {
+            return res.requestError({ code: 'VALIDATION', message: error });
+        }
+
+        if (!mongoose.validID(req.body.user_id)) {
+            return res.requestError({
+                code: "NOT_FOUND",
+                params: [ 'user_id' ]
+            });
+        }
+
+        query = {
+            _id: mongoose.Types.ObjectId(req.body.user_id),
+            user_type: 'student',
+            status: 'active'
+        };
+        update_query = utils.clean({
+            first_name: req.body.first_name,
+            last_name:  req.body.last_name,
+            status: 'active'
+        });
+        return user_model.find(query).exec().then(function(ret) {
+            if (!ret.length) {
+                return Promise.reject({
+                        code: "NOT_FOUND",
+                        params: [ 'user_id' ]
+                    });
+            }
+            return user_model.findOneAndUpdate(query, update_query)
+                .exec();
+        }).then(function(ret) {
+            res.sendResponse(ret._id);
+        }).catch(function (err) {
+            res.requestError(err);
+        });
+
+    }).delete(function (req, res, next) {
+        var error,
+            query;
+        if (req.session_user_type !== 'admin') {
+            return res.requestError({
+                code: 'NOT_FOUND',
+                params: [ 'user_id' ]
+            });
+        }
+        validator.validate(req.body, student_delete_schema);
+        error = validator.getLastErrors();
+        if (error) {
+            return res.requestError({ code: "VALIDATION", message: error });
+        }
+        query = {
+            _id: mongoose.Types.ObjectId(req.body.user_id),
+            user_type: 'student'
+        };
+        return user_model.find(query).exec().then(function(ret) {
+            if (!ret.length) {
+                return Promise.reject({
+                        code: "NOT_FOUND",
+                        params: [ 'user_id' ]
+                    });
+            }
+            return user_model.findOneAndUpdate(query, {
+                    status: 'inactive'
+                }).exec();
+        }).then(function(ret) {
+            res.sendResponse(ret._id);
+        }).catch(function (err) {
+            res.requestError(err);
+        });
+    }).all(function (req, res, next) {
+        return res.invalidVerb();
+    });
+
+    router.route('/all').get(function(req, res, next) {
+        var error,
+            query;
+        if (req.session_user_type !== 'admin') {
+            return res.forbidden();
+        }
+        if (req.query.student_number) {
+            req.query.student_number = sanitizer.sanitize(req.query.student_number,
+                'stringToInteger');
+        }
+        validator.validate(req.query, student_all_get_schema);
+        error = validator.getLastErrors();
+        if (error) {
+            return res.requestError({ code: "VALIDATION", message: error });
+        }
+        query = {
+            email:      req.query.email,
+            first_name: req.query.first_name,
+            last_name:  req.query.last_name,
+            utorid:     req.query.utorid,
+            status:     req.query.status,
+            user_type:  'student',
+            student_number: req.query.last_name
+        };
+        if (req.query.user_id) {
+            if (!mongoose.validID(req.query.user_id)) {
+                return res.sendResponse([]);
+            }
+            query._id = mongoose.Types.ObjectId(req.query.user_id);
+        }
+        utils.clean(query);
+        return user_model.aggregate([
+            { $match: query },
+            { $project : {
+                user_id: "$_id",
+                _id: 0,
+                email: 1,
+                first_name: 1,
+                last_name: 1,
+                utorid: 1,
+                status: 1,
+                student_number: 1,
+                last_login: { 
+                    $dateToString: { 
+                        format: "%Y-%m-%d %H:%M:%S", 
+                        date: "$last_login" 
+                    }
+                }
+            }
+        }
+        ]).exec().then(function(ret) {
+            return res.sendResponse(ret);
+        }).catch(function(err) {
+            res.requestError(err);
+        });
+    }).all(function(req, res, next) {
+        return res.invalidVerb();
+    });
+};
