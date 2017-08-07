22818a8a2f2e0bc0f4cdf3484ea4c9d3ecb39dfd
Vincent Tse <from_vincent@hotmail.com>
2017-08-07 08:39:04 -0400

    fixed getToReview, getReviewBy. add getCode

--- angular-front-end/src/app/api.service.ts
+++ angular-front-end/src/app/api.service.ts
@@ -34,10 +34,38 @@ export class ApiService {
             .put("http://localhost:3000/api/works", JSON.stringify({name: name, num_peers: num_peers}), {headers: this.headers});
     }
 
+    // return a list of user_id that is going to review
+    getToReview(work_id:string, user_id: string) {
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('work_id', work_id);
+        params.set('review_by', user_id);
+
+        let requestOptions = new RequestOptions();
+        requestOptions.params = params;
+
+        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
+            .map(res => res.json().data)
+            .map(res => res.author);
+    }
+
+    // return a list of user_id that are reviewed by
+    getReviewBy(work_id:string, user_id: string) {
+        let params: URLSearchParams = new URLSearchParams();
+        params.set('work_id', work_id);
+        params.set('author', user_id);
+
+        let requestOptions = new RequestOptions();
+        requestOptions.params = params;
+
+        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
+            .map(res => res.json().data)
+            .map(res => res.review_by);
+    }
 
     // get all the submissions belongs to student 
-    getSubmissions(user_id: string) {
+    getSubmissions(work_id: string, user_id: string) {
         let params: URLSearchParams = new URLSearchParams();
+        params.set('work_id', work_id);
         params.set('user_id', user_id);
 
         let requestOptions = new RequestOptions();
@@ -47,17 +75,32 @@ export class ApiService {
             .map(res => res.json().data);
     }
 
-    // get one submission 
-    getOneSubmission(sub_id: string) {
+    // get the actual code of the submission
+    getCode(subf_id: string) {
         let params: URLSearchParams = new URLSearchParams();
-        params.set('submission_id', sub_id);
+        params.set('submission_id', subf_id);
 
         let requestOptions = new RequestOptions();
         requestOptions.params = params;
 
-        return this.http.get("http://localhost:3000/api/works/submissions", requestOptions)
-            .map(res => res.json().data);    
+        return this.http.get("http://localhost:3000/api/works/submissions/files", requestOptions)
+            .map(res => res.json().data);
     }
+
+
+
+
+    // // get one submission 
+    // getOneSubmission(sub_id: string) {
+    //     let params: URLSearchParams = new URLSearchParams();
+    //     params.set('submission_id', sub_id);
+
+    //     let requestOptions = new RequestOptions();
+    //     requestOptions.params = params;
+
+    //     return this.http.get("http://localhost:3000/api/works/submissions", requestOptions)
+    //         .map(res => res.json().data);    
+    // }
 
     // get all annotations of one submission
     getAnnotations(user_id: string, sub_id: string) {
@@ -73,44 +116,6 @@ export class ApiService {
     }
 
 
-    // // get the actual code of the submission
-    // getCode(subf_id: string) {
-    //     let params: URLSearchParams = new URLSearchParams();
-    //     params.set('submission_id', sub_id);
-
-    //     let requestOptions = new RequestOptions();
-    //     requestOptions.params = params;
-    //     const query = {submission_file_id : subf_id};
-    //     return this.http.get("http://localhost:3000/api/works/submissions/files", requestOptions)
-    //         .map(res => res.json().data);
-    // }
-
-    // get to review list
-    getToReview(work_id:string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('work_id', work_id);
-        params.set('review_by', user_id);
-
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
-
-        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
-            .map(res => res.json().data);
-    }
-
-    // get review_by list
-    getReviewBy(work_id:string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('work_id', work_id);
-        params.set('author', user_id);
-
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
-
-        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
-            .map(res => res.json().data);
-    }
-
     // get all the feedbacks of on submission
     getFeedbacks(work_id: string, sub_id: string) {
         let params: URLSearchParams = new URLSearchParams();
@@ -119,8 +124,7 @@ export class ApiService {
 
         let requestOptions = new RequestOptions();
         requestOptions.params = params;
-        const query = {submission_id : sub_id, review_by : user_id};
-        return this.http.get("http://localhost:3000/api/works/annotations", query)
+        return this.http.get("http://localhost:3000/api/works/feedbacks/all", query)
             .map(res => res.json().data);
     }
 
