c9eb0d746de45d962a430c713e7066d5fb2f65d1
Andrew Wang <me@andrewwang.ca>
2017-08-07 01:14:44 -0400

    Update navbar behaviour

--- angular-front-end/src/app/navbar.component.ts
+++ angular-front-end/src/app/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 import { OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
@@ -11,60 +13,83 @@ import { OnInit } from '@angular/core';
 
 
 export class NavbarComponent {
-  // color of buttons
   ngOnInit() {
-    // ...
-  }
-  color = ["Red", "Orange", "Yellow", "Olive", "Green"];
-  username = "The Student's Name"
-  tabs = [{
-    tab_name: "Submit",
-    sub_tab_names: [
-      {
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 0"
-      },{
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 1"
-      },{
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 2"
-      }
-    ]
-  },{
-    tab_name: "Reflect",
-    sub_tab_names: [
-      {
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 0"
-      },{
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 1"
-      }
-    ]
-  }, {
-    tab_name: "Review",
-        sub_tab_names: [
-      {
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 0"
-      },{
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 1"
-      }
-    ]
-  }, {
-    tab_name: "Results",
-        sub_tab_names: [
-      {
-        id: "workId:kkasdlkfjasldkfj",
-        name: "Assignment 0"
-      }
-    ]
-  }
-  ]
+    var stageMapping = {
+        'Submit': 'submission',
+        'Reflect': '',
+        'Review': 'j-review',
+        'Results': '',
+        'Admin stuff': 'admin',
+    };
 
-  on_click = console.log;
+    Object.keys(stageMapping).forEach(function(nextStage) {
+      if (stageMapping[nextStage]) {
+        $(stageMapping[nextStage]).hide();
+      }
+    });
+  }
+
+  username = 'Student Name';
+  tabs = [
+    {
+      tab_name: 'Submit',
+      sub_tab_names: [
+        {
+          value: 'a1',
+          name: 'Assignment 1',
+        },
+      ],
+    },
+    {
+      tab_name: 'Reflect',
+      sub_tab_names: [
+        {
+          value: 'a1',
+          name: 'Assignment 1',
+        },
+      ],
+    },
+    {
+      tab_name: 'Review',
+      sub_tab_names: [
+        {
+          value: 'a1',
+          name: 'Assignment 1',
+        },
+      ],
+    },
+    {
+      tab_name: 'Results',
+      sub_tab_names: [
+        {
+          id: 'a1',
+          name: 'Assignment 1',
+        },
+      ],
+    },
+  ];
+
+  onClick = function(stage, workId) {
+    var stageMapping = {
+        'Submit': 'submission',
+        'Reflect': '',
+        'Review': 'j-review',
+        'Results': '',
+        'Admin stuff': 'admin',
+    };
+
+    if (stageMapping[stage]) {
+      Object.keys(stageMapping).forEach(function(nextStage) {
+        if (nextStage === stage) {
+          $(stageMapping[nextStage]).show();
+        } else {
+          $(stageMapping[nextStage]).hide();
+        }
+      });
+    } else {
+      alert('Comming soon');
+    }
+  }
 
   constructor (private http: Http) {
   }
