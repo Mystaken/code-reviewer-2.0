4da3f186e2ea8323d888c2e2f5ad5c73722a291c
Vincent Tse <from_vincent@hotmail.com>
2017-08-06 21:23:32 -0400

    add new api calls

--- angular-front-end/src/app/api.service.ts
+++ angular-front-end/src/app/api.service.ts
@@ -36,5 +36,42 @@ export class ApiService {
 
 
 
+    // get all the submissions belongs to student 
+    getSubmissions(user_id: string) {
+        const query = {author_id: user_id};
+        return this.http.get("http://localhost:3000/api/works/submissions/all", query)
+            .map(res => res.json().data);
+    }
+
+
+    // @kevin this might be helpfull, change them it they are wrong 
+    getOneSubmission(sub_id: string) {
+        const query = {submission_id : sub_id}; // hardcode sub_id
+        return this.http.get("http://localhost:3000/api/works/submissions", query)
+            .map(res => res.json().data);    
+    }
+
+    // get all annotations of one submission
+    getAnnotations(user_id: string, sub_id: string) {
+        const query = {submission_id : sub_id, review_by : user_id};
+        return this.http.get("http://localhost:3000/api/works/annotations", query)
+            .map(res => res.json().data);
+    }
+
+
+    // get the actual code of the submission
+    getCode(subf_id: string) {
+        const query = {submission_file_id : subf_id};
+        return this.http.get("http://localhost:3000/api/works/submissions/files", query)
+            .map(res => res.json().data);
+    }
+
+    // // get all the feedbacks of on submission
+    // getFeedbacks(user_id: string, sub_id: string) {
+    //     const query = {submission_id : sub_id, review_by : user_id};
+    //     return this.http.get("http://localhost:3000/api/works/annotations", query)
+    //         .map(res => res.json().data);
+    // }
+
 
 }
