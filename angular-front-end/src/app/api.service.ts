cc0604a219a437c434ea0dbcbc8f782d6662369f
Andrew Wang <me@andrewwang.ca>
2017-08-08 23:55:40 -0400

    Add delete button to work table

--- angular-front-end/src/app/api.service.ts
+++ angular-front-end/src/app/api.service.ts
@@ -5,145 +5,152 @@ import { Observable } from 'rxjs/Rx';
 @Injectable()
 export class ApiService {
 
-    private headers = new Headers({'Content-Type': 'application/json'});
+  private headers = new Headers({'Content-Type': 'application/json'});
 
-    constructor(private http: Http) { 
-    }
+  constructor(private http: Http) {
+  }
 
 
-    getPost(): Observable<any> {
-        return this.http.get("https://jsonplaceholder.typicode.com/posts")
-            .map(res => res.json());
-    }
+  getPost(): Observable<any> {
+    return this.http.get("https://jsonplaceholder.typicode.com/posts")
+      .map(res => res.json());
+  }
 
-    // get all the students
-    getStudents() {
-        return this.http.get("http://localhost:3000/api/users/students/all")
-            .map(res => res.json().data);
-    }
+  // get all the students
+  getStudents() {
+    return this.http.get("http://localhost:3000/api/users/students/all")
+      .map(res => res.json().data);
+  }
 
-    // get all the works
-    getWorks() {
-        return this.http.get("http://localhost:3000/api/works/all")
-            .map(res => res.json().data);
-    }
+  // get all the works
+  getWorks() {
+    return this.http.get("http://localhost:3000/api/works/all")
+      .map(res => res.json().data);
+  }
 
-    // create a new work
-    createNewWork(name: string, num_peers: number) {
-        let headers = new Headers({'Content-Type': 'application/json'});
-        return this.http
-            .put("http://localhost:3000/api/works", JSON.stringify({name: name, num_peers: num_peers}), {headers: this.headers});
-    }
+  // create a new work
+  createNewWork(name: string, num_peers: number) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    return this.http
+      .put("http://localhost:3000/api/works", JSON.stringify({name: name, num_peers: num_peers}), {headers: this.headers});
+  }
 
-    // edit a new work
-    editWork(query) {
-        let headers = new Headers({'Content-Type': 'application/json'});
-        return this.http
-            .post("http://localhost:3000/api/works", JSON.stringify(query), {headers: this.headers});
-    }
+  // edit a new work
+  editWork(query) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    return this.http
+      .post("http://localhost:3000/api/works", JSON.stringify(query), {headers: this.headers});
+  }
 
-    // return a list of user_id that is going to review
-    getToReview(work_id:string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('work_id', work_id);
-        params.set('review_by', user_id);
+  // delete a work
+  deleteWork(query) {
+    let headers = new Headers({'Content-Type': 'application/json'});
+    return this.http
+      .delete("http://localhost:3000/api/works", {headers: this.headers, body: query});
+  }
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
+  // return a list of user_id that is going to review
+  getToReview(work_id:string, user_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('work_id', work_id);
+    params.set('review_by', user_id);
 
-        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
-            .map(res => res.json().data)
-            .map(res => res.author);
-    }
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-    // return a list of user_id that are reviewed by
-    getReviewBy(work_id:string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('work_id', work_id);
-        params.set('author', user_id);
+    return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
+      .map(res => res.json().data)
+      .map(res => res.author);
+  }
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
+  // return a list of user_id that are reviewed by
+  getReviewBy(work_id:string, user_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('work_id', work_id);
+    params.set('author', user_id);
 
-        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
-            .map(res => res.json().data)
-            .map(res => res.review_by);
-    }
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-    // return a list of user_id that is going to review
-    getToMark(work_id:string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('work_id', work_id);
-        params.set('mark_by', user_id);
+    return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
+      .map(res => res.json().data)
+      .map(res => res.review_by);
+  }
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
+  // return a list of user_id that is going to review
+  getToMark(work_id:string, user_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('work_id', work_id);
+    params.set('mark_by', user_id);
 
-        return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
-            .map(res => res.json().data)
-            .map(res => res.author);
-    }
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-    // get all the submissions belongs to student 
-    getSubmissions(work_id: string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('work_id', work_id);
-        params.set('user_id', user_id);
+    return this.http.get("http://localhost:3000/api/works/feedbacks/all", RequestOptions)
+      .map(res => res.json().data)
+      .map(res => res.author);
+  }
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
-        
-        return this.http.get("http://localhost:3000/api/works/submissions/all", requestOptions)
-            .map(res => res.json().data);
-    }
+  // get all the submissions belongs to student
+  getSubmissions(work_id: string, user_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('work_id', work_id);
+    params.set('user_id', user_id);
 
-    // get the actual code of the submission
-    getCode(subf_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('submission_file_id', subf_id);
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
+    return this.http.get("http://localhost:3000/api/works/submissions/all", requestOptions)
+      .map(res => res.json().data);
+  }
 
-        return this.http.get("http://localhost:3000/api/works/submissions/files", requestOptions)
-            .map(res => res.json().data);
-    }
+  // get the actual code of the submission
+  getCode(subf_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('submission_file_id', subf_id);
 
-    // get all annotations of one submission
-    getAnnotations(user_id: string, sub_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('submission_id', sub_id);
-        params.set('user_id', user_id);
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
+    return this.http.get("http://localhost:3000/api/works/submissions/files", requestOptions)
+      .map(res => res.json().data);
+  }
 
-        return this.http.get("http://localhost:3000/api/works/annotations", requestOptions)
-            .map(res => res.json().data);
-    }
+  // get all annotations of one submission
+  getAnnotations(user_id: string, sub_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('submission_id', sub_id);
+    params.set('user_id', user_id);
 
-    // get all the feedbacks of one submission
-    getFeedbacks(sub_id: string, user_id: string) {
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('submission_id', sub_id);
-        params.set('author', user_id);
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-        let requestOptions = new RequestOptions();
-        requestOptions.params = params;
+    return this.http.get("http://localhost:3000/api/works/annotations", requestOptions)
+      .map(res => res.json().data);
+  }
 
-        return this.http.get("http://localhost:3000/api/works/feedbacks/all", requestOptions)
-            .map(res => res.json().data);
-    }
+  // get all the feedbacks of one submission
+  getFeedbacks(sub_id: string, user_id: string) {
+    let params: URLSearchParams = new URLSearchParams();
+    params.set('submission_id', sub_id);
+    params.set('author', user_id);
 
-    // // get one submission 
-    // getOneSubmission(sub_id: string) {
-    //     let params: URLSearchParams = new URLSearchParams();
-    //     params.set('submission_id', sub_id);
+    let requestOptions = new RequestOptions();
+    requestOptions.params = params;
 
-    //     let requestOptions = new RequestOptions();
-    //     requestOptions.params = params;
+    return this.http.get("http://localhost:3000/api/works/feedbacks/all", requestOptions)
+      .map(res => res.json().data);
+  }
 
-    //     return this.http.get("http://localhost:3000/api/works/submissions", requestOptions)
-    //         .map(res => res.json().data);    
-    // }
+  // // get one submission
+  // getOneSubmission(sub_id: string) {
+  //   let params: URLSearchParams = new URLSearchParams();
+  //   params.set('submission_id', sub_id);
+
+  //   let requestOptions = new RequestOptions();
+  //   requestOptions.params = params;
+
+  //   return this.http.get("http://localhost:3000/api/works/submissions", requestOptions)
+  //     .map(res => res.json().data);
+  // }
 }
--- angular-front-end/src/app/work-table.component.ts
+++ angular-front-end/src/app/work-table.component.ts
@@ -4,97 +4,100 @@ import { ApiService } from './api.service'
 
 
 @Component({
-    selector: 'work-table',
-    templateUrl: './work-table.component.html',
-    styleUrls: ['./work-table.component.css']
+  selector: 'work-table',
+  templateUrl: './work-table.component.html',
+  styleUrls: ['./work-table.component.css']
 })
 
 
 export class WorkTableComponent {
 
-    rows: any[];
+  rows: any[];
 
-    constructor(private _apiService: ApiService) {
-        this.getWorks();
-    }
+  constructor(private _apiService: ApiService) {
+    this.getWorks();
+  }
 
-    getWorks() {
-        this._apiService
-            .getWorks()
-            .subscribe(data => this.rows = data);
-    }
+  getWorks() {
+    this._apiService
+      .getWorks()
+      .subscribe(data => this.rows = data);
+  }
 
-    ngOnInit() {
-        this.getWorks();
-    }
-
-
-    expanded: any = {};
-    timeout: any;
-    num_peers_options = [0, 1, 2, 3, 4, 5, 6, 7];
-    work_name = "";
-    num_peers = 0;
-
-    @ViewChild('myTable') table: any;
-
-    onPage(event) {
-        clearTimeout(this.timeout);
-        this.timeout = setTimeout(() => {
-            console.log('paged!', event);
-        }, 100);
-    }
-
-    toggleExpandRow(row) {
-        console.log(this.table.rowDetail);
-        console.log('Toggled Expand Row!', row);
-        this.work_name = row.name;
-        this.num_peers = row.num;
-        console.log(this.work_name);
-        this.table.rowDetail.toggleExpandRow(row);
-    }
-
-    onDetailToggle(event) {
-        console.log('Detail Toggled', event);
-    }
+  ngOnInit() {
+    this.getWorks();
+  }
 
 
-    // to remove row.$$expanded and row.$$index
-    createQuery(row) {
-        var query = {
-            name: row.name,
-            num_peers: row.num_peers,
-            status: row.status,
-            feedback_questions: row.feedback_questions,
-            required_files: row.required_files,
-            work_id: row.work_id
-        };
-        return query;
+  expanded: any = {};
+  timeout: any;
+  num_peers_options = [0, 1, 2, 3, 4, 5, 6, 7];
+  work_name = "";
+  num_peers = 0;
 
-    }
+  @ViewChild('myTable') table: any;
 
-    loadSubmission(row) {
-        //TODO:
-        // read file `row.repo_path + "/" + utorid + "/" + required_file` i forgot..
+  onPage(event) {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      console.log('paged!', event);
+    }, 100);
+  }
 
-        // you will need to call this._apiService.createSubmission(..)
-        // and this._apiService.createSubmissionFile(...)
-        // those two functions are not implemented.. I will do it when I wake up
-        // just assume they are there.
-        // or you can simple console what you read from file for testing
+  toggleExpandRow(row) {
+    console.log(this.table.rowDetail);
+    console.log('Toggled Expand Row!', row);
+    this.work_name = row.name;
+    this.num_peers = row.num;
+    console.log(this.work_name);
+    this.table.rowDetail.toggleExpandRow(row);
+  }
 
-        // show a message saying successful loaded..
+  onDetailToggle(event) {
+    console.log('Detail Toggled', event);
+  }
 
-    }
 
-    edit(row) {
-        console.log(row.work_id);
-        console.log(row);
-        delete row.status;
-        return this._apiService
-            .editWork(this.createQuery(row))
-            .subscribe(function(data) {
+  // to remove row.$$expanded and row.$$index
+  createQuery(row) {
+    var query = {
+      name: row.name,
+      num_peers: row.num_peers,
+      status: row.status,
+      feedback_questions: row.feedback_questions,
+      required_files: row.required_files,
+      work_id: row.work_id
+    };
+    return query;
 
-            });
-    }
+  }
 
+  loadSubmission(row) {
+    //TODO:
+    // read file `row.repo_path + "/" + utorid + "/" + required_file` i forgot..
+
+    // you will need to call this._apiService.createSubmission(..)
+    // and this._apiService.createSubmissionFile(...)
+    // those two functions are not implemented.. I will do it when I wake up
+    // just assume they are there.
+    // or you can simple console what you read from file for testing
+
+    // show a message saying successful loaded..
+
+  }
+
+  edit(row) {
+    delete row.status;
+    return this._apiService
+      .editWork(this.createQuery(row))
+      .subscribe(function(data) {
+      });
+  }
+
+  delete(row) {
+    return this._apiService
+      .deleteWork(this.createQuery({'work_id': row.work_id}))
+      .subscribe(function(data) {
+      });
+  }
 }
