3bc5fb9002059ef0ba96d74524cd9ea58098a6ac
Kevin Gao <tianxiang.gao.1996@gmail.com>
2017-08-11 21:50:41 -0500

    get now returns singleton

--- back-end-API/routes/works/submissions/index.js
+++ back-end-API/routes/works/submissions/index.js
@@ -64,7 +64,7 @@ module.exports = function (router) {
                         params: [ 'submission_id' ]
                     });
             }
-            return res.sendResponse(ret);
+            return res.sendResponse(ret[0]);
         }).catch(function(err) {
             return res.requestError(err);
         });
