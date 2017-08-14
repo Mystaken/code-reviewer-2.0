20158980d57cd1bd53ffdaa22753dc8a75c76f2a
Kevin Gao <tianxiang.gao.1996@gmail.com>
2017-08-14 14:47:44 -0400

    Create auth-guard.service.ts

---
+++ angular-front-end-2/src/app/guards/auth-guard.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
+import { SessionUserService } from '../services/session-user.service';
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private _userService: SessionUserService, private router: Router) {}
+  canActivate() {
+  }
+}
