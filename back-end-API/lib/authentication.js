53c0e763f9f839fcad8d45d067ac9cdb785b4cbf
BoZhaoUTSC <bodotzhao@gmail.com>
2017-11-04 01:38:45 -0400

    Authenticate using custom access_token to avoid exceeding auth0 rate limit

--- back-end-API/lib/authentication.js
+++ back-end-API/lib/authentication.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var request     = require('request-promise'),
-    user_model  = require('../models/users');
+    jwt         = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
     // find access_token
@@ -12,35 +12,17 @@ module.exports = function(req, res, next) {
         access_token = req.body.access_token;
     // if access_token is found
     if (access_token) {
-        // send a get request to auth0 to retrive uesr email
-        var options = {
-            uri: 'https://' + process.env.DOMAIN + '/userinfo',
-            headers: {
-                'Authorization': 'Bearer ' + access_token
-            },
-            json: true // Automatically parses the JSON string in the response
-        };
-        return request(options)
-            .then(function (result) {
-                // if this user has a valid access_token
-                if (result !== null) {
-                    // get this user's user_id and user_type by his email
-                    return user_model.findOne({email: result.email}, "_id user_type", function(err, user) {
-                        if (err) return res.forbidden();
-                        if (user === null) return res.forbidden();
-                        // set this user's user_id and user_type
-                        req.session_user_id = user._id.toString();
-                        req.session_user_type = user.user_type;
-                        next();
-                    });
-                // if this user does not have a valid access_token, i.e. can't get his email
-                } else {
-                    res.forbidden();
-                }
-            })
-            .catch(function (err) {
-                return res.forbidden();
-            });
+        
+        // verify custom access_token by CLIENT_SECRET
+        // TODO: check expire time
+        try {
+            var decoded = jwt.verify(access_token, process.env.CLIENT_SECRET);
+            req.session_user_id = decoded.user_id;
+            req.session_user_type = decoded.user_type;
+            next();
+        } catch (err) {
+            res.forbidden();
+        }
     } else {
         return res.forbidden();
     }
--- back-end-API/routes/login/index.js
+++ back-end-API/routes/login/index.js
@@ -3,7 +3,9 @@
 var validator   = require('../../lib/validator'),
     utils       = require('../../lib/utils'),
     Promise     = require('bluebird'),
-    request     = require('request')
+    request     = require('request'),
+    jwt         = require('jsonwebtoken'),
+    user_model  = require('../../models/users');
 
 
 module.exports = function (router) {
@@ -11,7 +13,7 @@ module.exports = function (router) {
     router.route('/').post(function(req, res, next) {
         // if post.body contains email and password
         if (req.body && req.body.email && req.body.password) {
-            // get auth0 access_token by email and password
+            // get auth0 access_token by email and password from auth0/userinfo
             var options = {
                 method: 'POST',
                 url: 'https://' + process.env.DOMAIN + '/oauth/token',
@@ -27,11 +29,25 @@ module.exports = function (router) {
                 },
                 json: true
             };
-
             // get token, expire_at and send it back to the user
             request(options, function (error, response, body) {
                 if (error) throw new Error(error);
-                return res.sendResponse(body)
+                // get this user's user_id and user_type by email
+                user_model.findOne({email: req.body.email}, "_id user_type", function(err, user) {
+                    if (err) return res.forbidden();
+                    // if there's no user with this email in mongoDB
+                    if (user === null) return res.forbidden();
+                    
+                    // set this user's user_id and user_type
+                    var custom_access_token = jwt.sign({
+                        user_id: user._id.toString(),
+                        user_type: user.user_type
+                    }, process.env.CLIENT_SECRET, { expiresIn: '24h'});
+
+                    return res.sendResponse({
+                        access_token: custom_access_token
+                    });
+                });
             });
         }
     }).all(function (req, res, next) {
