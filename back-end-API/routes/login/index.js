dbe7037fff5df5fefb2f4350eecdc56662154d16
Bo Zhao <bodotzhao@gmail.com>
2018-08-19 21:00:33 -0400

    Update stimulate dev env and README

--- back-end-API/routes/login/index.js
+++ back-end-API/routes/login/index.js
@@ -1,54 +1,70 @@
-'use strict';
+"use strict";
 
-var validator   = require('../../lib/validator'),
-    utils       = require('../../lib/utils'),
-    Promise     = require('bluebird'),
-    jwt         = require('jsonwebtoken'),
-    bcrypt      = require('bcrypt'),
-    user_model  = require('../../models/users'),
-    authentication_model = require('../../models/authentication');
+var validator = require("../../lib/validator"),
+  utils = require("../../lib/utils"),
+  Promise = require("bluebird"),
+  jwt = require("jsonwebtoken"),
+  bcrypt = require("bcrypt"),
+  user_model = require("../../models/users"),
+  authentication_model = require("../../models/authentication"),
+  config = require("../../config/config.json");
 
-module.exports = function (router) {
+module.exports = function(router) {
+  router
+    .route("/")
+    .post(function(req, res, next) {
+      // if post.body contains email and password
+      if (req.body && req.body.email && req.body.password) {
+        // find the user by email
+        authentication_model.findOne(
+          {
+            email: req.body.email
+          },
+          function(err, auth) {
+            if (err) throw err;
+            // no user with this pasword exists in db
+            if (!auth) {
+              return res.sendResponse({
+                message: "Authentication failed. Email not found."
+              });
+            }
+            // assume user exists, check password
+            if (!bcrypt.compareSync(req.body.password, auth.hash)) {
+              return res.sendResponse({
+                message: "Authentication failed. Wrong password."
+              });
+            }
+            // assume password is correct
+            // get this user's user_id and user_type by email
+            user_model.findOneAndUpdate(
+              { email: req.body.email },
+              { $set: { last_login: new Date() } },
+              function(err, user) {
+                if (err) return res.forbidden();
+                // if there's no user with this email in mongoDB
+                if (user === null) return res.forbidden();
+                // use dev secret if prod secret is not found
+                var secret = process.env.SECRET || config.secret;
+                // set this user's user_id and user_type
+                var custom_access_token = jwt.sign(
+                  {
+                    user_id: user._id.toString(),
+                    user_type: user.user_type
+                  },
+                  secret,
+                  { expiresIn: "24h" }
+                );
 
-    router.route('/').post(function(req, res, next) {
-        // if post.body contains email and password
-        if (req.body && req.body.email && req.body.password) {
-            // find the user by email
-            authentication_model.findOne({
-                email: req.body.email
-            }, function(err, auth) {
-                if (err) throw err;
-                // no user with this pasword exists in db
-                if (!auth) {
-                    return res.sendResponse({
-                        message: "Authentication failed. Email not found."
-                    });
-                }
-                // assume user exists, check password
-                if (!bcrypt.compareSync(req.body.password, auth.hash)) {
-                    return res.sendResponse({
-                        message: "Authentication failed. Wrong password."
-                    });
-                }
-                // assume password is correct
-                // get this user's user_id and user_type by email
-                user_model.findOneAndUpdate({email: req.body.email}, { $set: { last_login: new Date() }}, function(err, user) {
-                    if (err) return res.forbidden();
-                    // if there's no user with this email in mongoDB
-                    if (user === null) return res.forbidden();
-                    // set this user's user_id and user_type
-                    var custom_access_token = jwt.sign({
-                        user_id: user._id.toString(),
-                        user_type: user.user_type
-                    }, process.env.SECRET, { expiresIn: '24h'});
-
-                    return res.sendResponse({
-                        access_token: custom_access_token
-                    });
+                return res.sendResponse({
+                  access_token: custom_access_token
                 });
-            });
-        }
-    }).all(function (req, res, next) {
-        return res.invalidVerb();
+              }
+            );
+          }
+        );
+      }
+    })
+    .all(function(req, res, next) {
+      return res.invalidVerb();
     });
 };
